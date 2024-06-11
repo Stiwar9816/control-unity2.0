4b51fd951810a1d239410f953469ada83539d0be
JhonE17 <jhonepalacios17@gmail.com>
2024-06-10 21:51:33 -0500

    delete (/utils/*) files moved from directory

--- src/utils/formatBytes.ts
+++
@@ -1,4 +0,0 @@
-export const formatBytes = (bytes: number) => {
-  const kb = bytes / 1024
-  return kb < 1024 ? kb.toFixed(2) + ' KB' : (kb / 1024).toFixed(2) + ' MB'
-}
--- src/utils/randomPassword.ts
+++
@@ -1,8 +0,0 @@
-export const randomPassword = (value: number) => {
-  const letters = '0123456789ABCDEFGHIJKLMNÑOPQRSTUVXYZabcdefghijklmnñopqrtuvwxyz*-/!#$%&_+¡'
-  let password = ''
-  for (let i = 0; i < value; i++) {
-    password += letters[Math.floor(Math.random() * 73)]
-  }
-  return password
-}
