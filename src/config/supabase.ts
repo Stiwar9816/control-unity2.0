ea2f0c85cb4463947f01bbecd1d62d98afc863f3
JhonE17 <jhonepalacios17@gmail.com>
2024-06-10 21:52:21 -0500

    refactor (/config) files moved from utils directory to confing directory

---
+++ src/config/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseServiceRolekey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseServiceRolekey, {
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true
+  }
+})
